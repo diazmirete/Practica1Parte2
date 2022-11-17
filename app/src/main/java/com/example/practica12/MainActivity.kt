35ee3203a95dc7ec26c88c4b9e275733049316a7
Ignacio Diaz <mirete.casem@gmail.com>
2022-11-17 17:46:57 +0100

    Commit final

--- app/src/main/java/com/example/practica12/MainActivity.kt
+++ app/src/main/java/com/example/practica12/MainActivity.kt
@@ -11,11 +11,21 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
     private lateinit var etClave: EditText
     private lateinit var btAceptar: Button
+    fun comprobarClave() {
+        if (etClave.getText().toString().equals("1234")) {
+            val intent = Intent(this, PruebaClave::class.java)
+            startActivity(intent)
+        } else {
+            //enviamos un mensaje por pantalla
+            Toast.makeText(this, getString(R.string.msj_incorrecta), Toast.LENGTH_LONG).show()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         etClave=findViewById(R.id.etClave)
         btAceptar=findViewById(R.id.btAceptar)
+        btAceptar.setOnClickListener{comprobarClave()}
     }
 }
 
