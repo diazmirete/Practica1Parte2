17e00838c6f87a66241e65a448e2dee52eacbc52
Ignacio Diaz <mirete.casem@gmail.com>
2022-11-17 16:43:54 +0100

    Commit 1

--- app/src/main/java/com/example/practica12/MainActivity.kt
+++ app/src/main/java/com/example/practica12/MainActivity.kt
@@ -2,8 +2,13 @@ package com.example.practica12
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var etClave: EditText
+    private lateinit var btAceptar: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
